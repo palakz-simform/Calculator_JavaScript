944ce5fec1743fcbffe80f1bae081551b4e1d2ae
Palak Zalavadia <palak.z@simformsolutions.com>
2023-02-01 13:52:37 +0530

    Code Formatted

--- script.js
+++ script.js
@@ -1,190 +1,179 @@
-const actions = document.querySelector('.actions');
+const actions = document.querySelector(".actions");
+const ans = document.getElementById("input");
+let expression = "";
+actions.addEventListener("click", (e) => {
+  console.log(e.target);
+  const value = e.target.value || e.target.parentElement.value;
 
-const ans = document.getElementById('input');
+  if (value !== undefined) {
+    switch (value) {
+      case "DEG":
+      case "RAD":
+        degrad();
+        break;
 
-let expression = '';
+      case "fe":
+        expression = parseFloat(expression).toExponential();
+        break;
+      case "mc":
+        memory = 0;
+        expression = memory;
+        break;
 
-actions.addEventListener('click', (e) => {
-console.log(e.target);
-const value = e.target.value||e.target.parentElement.value;
-  
-if(value !== undefined) {
-	
-	switch(value){
-		case "DEG":
-		case "RAD":
-			degrad();
-			break;
+      case "mr":
+        let finalans = expression + memory;
+        expression = eval(finalans);
+        break;
 
-		case "fe":
-			expression = parseFloat(expression).toExponential();
-			break;
-		case "mc":
-			memory=0
-			expression=memory;
-			break;
+      case "ms":
+        memory = expression;
+        expression = 0;
+        document.getElementById("mc").disabled = false;
+        document.getElementById("mr").disabled = false;
+        break;
 
-		case "mr":
-				let finalans = expression+memory; 
-				expression = eval(finalans);
-				break;
+      case "m+":
+        memory = memory + expression;
+        expression = 0;
+        break;
 
-		case "ms":
-			memory = expression;
-			expression=0;
-			document.getElementById("mc").disabled=false;
-			document.getElementById("mr").disabled=false;
-			break;
+      case "m-":
+        memory = memory - expression;
+        expression = 0;
+        break;
 
-		case "m+":
-			memory = memory+expression;
-			expression=0;
-			break;
-		
-		case "m-":
-			memory = memory-expression;
-			expression=0;
-			break;
+      case "log2":
+        expression = Math.log2(expression);
+        break;
 
-		
-		case "log2":
-			expression = Math.log2(expression)
-			break;
+      case "rnd":
+        expression = Math.random();
+        break;
 
-		case "rnd":
-			expression = Math.random()
-			break;
+      case "log1p":
+        expression = Math.log1p(expression);
+        break;
 
-		case "log1p":
-			expression = Math.log1p(expression)
-			break;
+      case "2^n":
+        expression = Math.pow(2, expression);
+        break;
 
-		case "2^n":
-			expression = Math.pow(2,expression) 
-			break;
+      case "3^n":
+        expression = Math.pow(3, expression);
+        break;
 
-		case "3^n":
-			expression = Math.pow(3,expression) 
-			break;
-		case "n^3":
-			expression = Math.pow(expression,3) 
-			break;
-	
-		case "3root":
-			expression = Math.cbrt(expression) 
-			break;
-		case "pi":
-			expression=Math.PI*expression;
-			break;
+      case "n^3":
+        expression = Math.pow(expression, 3);
+        break;
 
-		case "ce":
-			expression = '';
-			ans.value = 0;
-			break;
-					
-		case "clear":
-			let exp = expression
-			if(exp.length>0){
-				exp = exp.substring(0,exp.length-1);
-				expression = exp;
-			}
-			break;
+      case "3root":
+        expression = Math.cbrt(expression);
+        break;
+      case "pi":
+        expression = Math.PI * expression;
+        break;
 
-		case "square":
-			expression=Math.pow(expression,2);
-			break;
+      case "ce":
+        expression = "";
+        ans.value = 0;
+        break;
 
-		case "1/x":
-			expression = 1/expression
-			break;
+      case "clear":
+        let exp = expression;
+        if (exp.length > 0) {
+          exp = exp.substring(0, exp.length - 1);
+          expression = exp;
+        }
+        break;
 
-		case "|x|":
-			expression = Math.abs(expression);
-			break
+      case "square":
+        expression = Math.pow(expression, 2);
+        break;
 
-		case "exp":
-			expression = Math.exp(expression)
-			break;
+      case "1/x":
+        expression = 1 / expression;
+        break;
 
-		case "sqrt":
-			expression = (Math.sqrt(expression));
+      case "|x|":
+        expression = Math.abs(expression);
+        break;
 
-		case "factorial":
-			if(expression === 0){
-				expression = 1;
-			}
-			else{
-				let fact = 1;
-				for (i=1;i<=expression;i++){
-					fact *= i;
-				}
-				expression = fact
-			}
+      case "exp":
+        expression = Math.exp(expression);
+        break;
 
-		case "=":
-			const answer = eval(expression);
-			expression = answer;
-			break;
-		
-		case "10power":
-			expression = 10**expression;
-			break;
+      case "sqrt":
+        expression = Math.sqrt(expression);
 
-		case "ln":
-			expression = Math.log(expression)
-			break;
-		
-		case "log10":
-			expression = Math.log10(expression)
-			break;
+      case "factorial":
+        if (expression === 0) {
+          expression = 1;
+        } else {
+          let fact = 1;
+          for (i = 1; i <= expression; i++) {
+            fact *= i;
+          }
+          expression = fact;
+        }
 
-		case "sin":
-			expression = Math.sin(expression);
-			break;
+      case "=":
+        const answer = eval(expression);
+        expression = answer;
+        break;
 
-		case "cos":
-			expression = Math.cos(expression);
-			break;
+      case "10power":
+        expression = 10 ** expression;
+        break;
 
-		case "tan":
-			expression = Math.tan(expression);
-			break;
+      case "ln":
+        expression = Math.log(expression);
+        break;
 
-		case "+/-":
-			if(expression>0){
-				expression = expression*(-1)
-			}
-			else{
-				expression = Math.abs(expression)
-			}
-			break;
+      case "log10":
+        expression = Math.log10(expression);
+        break;
 
-		default:
-			expression += value;
-	}
+      case "sin":
+        expression = Math.sin(expression);
+        break;
 
-	function degrad(){
-		var t = document.getElementById("rd");
-		
-		if(t.value==="DEG"){
-			expression = expression * (180/Math.PI)
-			t.value="RAD"
-		}
-		else{
-			expression = expression * (Math.PI/180)
-			t.value="DEG"
-		}
+      case "cos":
+        expression = Math.cos(expression);
+        break;
 
-	}
-	
-	if(expression == undefined) {
-		expression = '';
-		ans.value = 0;
-	} else {
-		ans.value = expression;
-	}			
-	
-	}
+      case "tan":
+        expression = Math.tan(expression);
+        break;
+
+      case "+/-":
+        if (expression > 0) {
+          expression = expression * -1;
+        } else {
+          expression = Math.abs(expression);
+        }
+        break;
+
+      default:
+        expression += value;
+    }
+
+    function degrad() {
+      var t = document.getElementById("rd");
+
+      if (t.value === "DEG") {
+        expression = expression * (180 / Math.PI);
+        t.value = "RAD";
+      } else {
+        expression = expression * (Math.PI / 180);
+        t.value = "DEG";
+      }
+    }
+
+    if (expression == undefined) {
+      expression = "";
+      ans.value = 0;
+    } else {
+      ans.value = expression;
+    }
+  }
 });
-
-
